c09bbcd398d5d62150dcb21160bf75401de66545
JeremyDuenas <ssjjhd@gmail.com>
2017-05-16 23:26:24 -0700

    Added predictions

--- regres.py
+++ regres.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import quandl
 import math
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume',]]
@@ -23,20 +28,33 @@ forecast_out = int(math.ceil(0.01*len(df)))
 print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
+
 
 # X = features
 X = np.array(df.drop(['label'],1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+
+
+df.dropna(inplace=True)
+
 # y = labels
 y = np.array(df['label'])
-X = preprocessing.scale(X)
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=.02)
 
-clf = LinearRegression()
+clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
 
 accuracy = clf.score(X_test,y_test)
 
-print(accuracy)
+forecast_set = clf.predict(X_lately)
+
+print (forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
+
+
+# print(accuracy)
