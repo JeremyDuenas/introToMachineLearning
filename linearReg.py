6ba4a0e2f5dfb35985190cccc6d7c2e3d4c97bd1
JeremyDuenas <ssjjhd@gmail.com>
2017-05-25 23:43:47 -0700

    creating datasets

--- linearReg.py
+++ linearReg.py
@@ -2,14 +2,25 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('fivethirtyeight')
 
 xValues = np.array([1,2,3,4,5,6], dtype=np.float64)
 yValues = np.array([5,4,6,5,6,7], dtype=np.float64)
 
-def create_dataset(h, variance, step=2, correlation=False):
-    return np.array(xs, dtype=np.float64), np.array(yValues, dtype=np.float64)
+def create_dataset(hm, variance, step=2, correlation=False):
+    val = 1;
+    yValues = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        yValues.append(y)
+        if correlation and correlation == 'pos':
+            val += step
+        elif correlation and correlation == 'neg':
+            val -= step
+    xValues = [i for i in range(len(yValues))]
+    return np.array(xValues, dtype=np.float64), np.array(yValues, dtype=np.float64)
 
 def best_fit_slope_and_intercept(xValues, yValues):
     m = ( ((mean(xValues) * mean(yValues)) - mean(xValues * yValues)) /
@@ -26,6 +37,7 @@ def coefficient_of_determination(ys_orig, ys_line):
     squared_error_y_mean = squared_error(ys_orig, y_mean_line)
     return 1 - (squared_error_regr / squared_error_y_mean)
 
+xValues, yValues = create_dataset(40, 40, 2, correlation='pos')
 m, b= best_fit_slope_and_intercept(xValues, yValues)
 
 print(m,b)
