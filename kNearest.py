7567b1b72a8b80d838f14d85a9062d1a9c79bcec
JeremyDuenas <ssjjhd@gmail.com>
2017-05-30 14:01:36 -0700

    testing against wisconsin breast cancer dataset

--- kNearest.py
+++ kNearest.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import warnings
 from matplotlib import style
 from collections import Counter
+import pandas as pd
+import random
+
 style.use('fivethirtyeight')
 
 plot1 = [1,3]
@@ -32,5 +35,33 @@ def k_nearest_neighbors(data, predict, k=3):
 
     return vote_result
 
-result = k_nearest_neighbors(dataset, new_features, k=3)
-print(result)
+df = pd.read_csv("breast-cancer-wisconsin.data")
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+full_data = df.astype(float).values.tolist()
+
+random.shuffle(full_data)
+
+test_size = 0.2
+train_set = {2:[], 4:[]}
+test_set = {2:[], 4:[]}
+
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k=5)
+        if group == vote:
+            correct +=1
+        total +=1
+
+print('Accuracy:', correct/total)
